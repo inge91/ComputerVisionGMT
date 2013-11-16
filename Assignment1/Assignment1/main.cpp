87fcfeea07b2d32a8d1364cb2796aec79cf113f5
Inge Becht <inge.becht91@gmail.com>
2013-11-16 17:48:59 +0100

    added chessboard calibration functions

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace cv;
 
+
 int main(int argc, char** argv)
 {
 	
@@ -13,13 +14,27 @@ int main(int argc, char** argv)
 	}
 
 	Mat img = imread(argv[1]);
+	
+	// Amount of inner corners of chessboard
+	CvSize patternSize = cvSize(9, 6);
 
+	// Not sure about this yet
+	std::vector<std::vector<cv::Point2f> > corners(1);
+
+
+	bool found = findChessboardCorners(img, patternSize, corners[0], CV_CALIB_CB_ADAPTIVE_THRESH);
+	drawChessboardCorners(img, patternSize, corners[0], found);
+	
+	
+	std::cout<<corners[0][0];
+	
 	// Get image size
 	Size s = img.size();
 	int centerX = s.width/2;
 	int centerY = s.height/2;
 
-	rectangle(img, Point(centerX - 50,centerY-50), Point(centerX + 50, centerY+50), Scalar(255, 255, 255));
+	//rectangle(img, Point(centerX - 50,centerY-50), Point(centerX + 50, centerY+50), Scalar(255, 255, 255));
+	rectangle(img, Point(corners[0][0].x, corners[0][0].y), Point(corners[0][0].x , corners[0][0].y ), Scalar(255, 255, 255));
 	
 	imshow("Assignment1", img);
 	cvWaitKey(0);
@@ -31,4 +46,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
