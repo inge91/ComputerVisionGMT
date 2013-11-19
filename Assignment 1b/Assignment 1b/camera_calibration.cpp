f5b2c30d8d8ae2da12b115ec7e91d91675ffc025
Inge Becht <inge.becht91@gmail.com>
2013-11-19 17:37:11 +0100

    now possible to create a basis, but not yet in right orientation gonna work on cleaning up the main

--- Assignment 1b/Assignment 1b/camera_calibration.cpp
+++ Assignment 1b/Assignment 1b/camera_calibration.cpp
@@ -560,6 +560,7 @@ bool runCalibrationAndSave(Settings& s, Size imageSize, Mat&  cameraMatrix, Mat&
     return ok;
 }
 
+
 int main(int argc, char* argv[])
 {
 	// Perform the calibration
@@ -578,10 +579,116 @@ int main(int argc, char* argv[])
 	fs["Camera_Matrix"] >> cameraMatrix;
 	std::cout<<cameraMatrix<<endl;
 
+	Mat eParam = Mat(4,4, CV_64F);
+
 	// Retrieve Extrinsic Parameters
 	Mat extrinsicParams;
 	fs["Extrinsic_Parameters"] >> extrinsicParams;
+	std::cout<<extrinsicParams<<endl;
+	Mat Img1P = extrinsicParams.row(0);
+	double x_r = Img1P.at<double>(0);
+	double y_r = Img1P.at<double>(1);
+	double z_r = Img1P.at<double>(2);
 
+	Mat rotX = Mat(3, 3, CV_64F);
+	rotX.row(0).col(0) = 1;
+	rotX.row(1).col(0) = 0;
+	rotX.row(2).col(0) = 0;
+	rotX.row(0).col(1) = 0;
+	rotX.row(1).col(1) = cos(x_r);
+	rotX.row(2).col(1) = -sin(x_r);
+	rotX.row(0).col(2) = 0;
+	rotX.row(1).col(2) = sin(x_r);
+	rotX.row(2).col(2) = cos(x_r);
+	std::cout<<rotX<<endl;
+
+	
+	Mat rotY = Mat(3, 3, CV_64F);
+	rotY.row(0).col(0) = cos(y_r);
+	rotY.row(1).col(0) = 0;
+	rotY.row(2).col(0) = -sin(y_r);
+	rotY.row(0).col(1) = 0;
+	rotY.row(1).col(1) = 1;
+	rotY.row(2).col(1) = 0;
+	rotY.row(0).col(2) = sin(y_r);
+	rotY.row(1).col(2) = 0;
+	rotY.row(2).col(2) = cos(y_r);
+	std::cout<<rotY<<endl;
+
+	Mat rotZ = Mat(3, 3, CV_64F);
+	rotZ.row(0).col(0) = cos(z_r);
+	rotZ.row(1).col(0) = -sin(z_r);
+	rotZ.row(2).col(0) = 0;
+	rotZ.row(0).col(1) = sin(z_r);
+	rotZ.row(1).col(1) = cos(z_r);
+	rotZ.row(2).col(1) = 0;
+	rotZ.row(0).col(2) = 0;
+	rotZ.row(1).col(2) = 0;
+	rotZ.row(2).col(2) = 1;
+	std::cout<<rotZ<<endl;
+
+
+	Mat r = Mat(2, 2, CV_64F);
+	r.row(0).col(0) = 1;
+	r.row(0).col(1) = 2;
+	r.row(1).col(0) = 0;
+	r.row(1).col(1) = 4;
+	Mat g = Mat(2, 2, CV_64F);
+	g.row(0).col(0) = 2;
+	g.row(0).col(1) = 4;
+	g.row(1).col(0) = 1;
+	g.row(1).col(1) = 3;
+
+	Mat f = rotX* rotY * rotZ;
+	
+	cout<<f.at<double>(0,0)<<endl;
+
+	eParam.row(0).col(0) =  f.at<double>(0,0);
+	eParam.row(0).col(1) =  f.at<double>(0,1); 
+	eParam.row(0).col(2) =  f.at<double>(0,2);
+	eParam.row(0).col(3) =  Img1P.at<double>(3);
+	eParam.row(1).col(0) =  f.at<double>(1,0);
+	eParam.row(1).col(1) =  f.at<double>(1,1);
+	eParam.row(1).col(2) =  f.at<double>(1,2);
+	eParam.row(1).col(3) =  Img1P.at<double>(4);
+	eParam.row(2).col(0) =  f.at<double>(2,0);
+	eParam.row(2).col(1) =  f.at<double>(2,1);
+	eParam.row(2).col(2) =  f.at<double>(2,2);
+	eParam.row(2).col(3) =  Img1P.at<double>(5);
+	eParam.row(3).col(0) =  0;
+	eParam.row(3).col(1) =  0;
+	eParam.row(3).col(2) =  0;
+	eParam.row(3).col(3) =  1;
+	
+	Mat vector = Mat(4, 1, CV_64F);
+	vector.row(0).col(0) = 100;
+	vector.row(1).col(0) = 0;
+	vector.row(2).col(0) = 0;
+	vector.row(3).col(0) = 1;
+
+	Mat camVec4 = eParam * vector;
+
+	cout<<"\n\n\n";
+	cout<<f<<endl;
+	cout<<eParam<<endl;
+	cout<<camVec4<<endl;
+
+	Mat camVec3 = Mat(3,1, CV_64F);
+	camVec3.row(0).col(0) = camVec4.at<double>(0,0);
+	camVec3.row(1).col(0) = camVec4.at<double>(1,0);
+	camVec3.row(2).col(0) = camVec4.at<double>(2,0);
+
+	Mat camVec5 =  cameraMatrix * camVec3;
+	Mat cameraCoords = Mat(3,1, CV_64F);
+ 
+	cameraCoords.row(0).col(0) =  camVec5.at<double>(0,0) / camVec5.at<double>(2,0);
+	cameraCoords.row(1).col(0) =  camVec5.at<double>(1,0) / camVec5.at<double>(2,0);
+	cameraCoords.row(2).col(0) =  camVec5.at<double>(2,0) / camVec5.at<double>(2,0);
+
+	cout<<"Camera coordinates: \n";
+	cout<<cameraCoords<<endl;
+	
+	
 	// Board points
 	Mat boardPoints;
 	fs["Image_points"] >> boardPoints;
@@ -595,15 +702,16 @@ int main(int argc, char* argv[])
 	Mat board2;
 	// Row chosen should depend on the image chosen
 	board1 = boardPoints.row(0);
+	// This is the point of the origin
+	double xm = 592;
+	double ym= 485;
 
-	// Draws all the points on the corners found
-	for(int j = 0; j < board1.cols*2; j+=2)
-	{
-		float x = board1.at<float>(0,j);
-		float y = board1.at<float>(0,j+1);
+	circle(image, Point(xm, ym), 10, cv::Scalar(50, 0, 0), 1);
 
-		circle(image, Point(x, y), 10, cv::Scalar(50, 0, 0), 1);
-	}
+	line(image, Point(xm, ym), Point(510, 469), Scalar(0, 0, 255), 3);
+	line(image, Point(xm, ym), Point(586, 383), Scalar(0, 0, 255), 3);
+	line(image, Point(xm, ym), Point(528, 513), Scalar(0, 0, 255), 3);
+
 	imshow("Assignment1", image);
 	waitKey(0);
 
